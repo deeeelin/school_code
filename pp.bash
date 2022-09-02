9f0f5e3b407588543c1bdcd2d0ac02d97608e21f
deeeelin <dereklin100503@gmail.com>
2022-09-02 18:25:01 +0800

    add file

--- pp.bash
+++ pp.bash
@@ -1,1 +1,36 @@
-alias eddgrrr
+#! /bin/bash
+
+function push() { 
+    
+    cd $(dirname $0)
+    git add .
+    git commit -m "add file"
+    git push school_code master:master
+    echo "completed"
+    return 
+
+
+}
+function pull() {
+    
+    cd $(dirname $0)
+    git pull school_code master
+    echo "completed"
+    return 
+}
+
+
+
+echo $1
+if [[ $1 == "p" ]];then
+    push
+    if [[ $? -eq 1 ]];then
+       echo "failed to push"
+    fi
+else
+    pull
+    if [[ $? -eq 1 ]];then
+       echo "failed to pull"
+    fi
+fi
+
